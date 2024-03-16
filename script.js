ce93e1a42809a88bb5d5b8d3b3ff3bf3e36f5ad4
vipuL2701 <vikratsethji@GMAIL.COM>
2024-03-16 10:51:38 +0530

    addfiles

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById('display');
+
+function addToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
